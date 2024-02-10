1a197b93679183dffec47df87285b629ea2a3eeb
AlexStich <alex@rucode.org>
2024-02-10 13:04:41 +0400

    Initial Commit

---
+++ ListOfWords/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  ListOfWords
+//
+//  Created by Aleksey Grebenkin on 10.02.24.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
